d5fb7ac1b73e2bfb729e27d0a965ee09e78fd641
thegreenvalley@yandex.ru <thegreenvalley@yandex.ru>
2023-05-23 16:44:26 +0500

    Добавление описания значений по умолчанию, и вывод результата работы скрипта

--- workin-with-files/copy-pdf-files.py
+++ workin-with-files/copy-pdf-files.py
@@ -4,17 +4,17 @@ import shutil
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 
-print("Укажите путь к папке, из которой скопировать файлы")
+print("Укажите путь к папке, из которой скопировать файлы\n(по умолчанию директория скрипта)")
 host_dir_path = input()
 if not host_dir_path:
     host_dir_path = current_path
 
-print("Укажите расширение файлов, которые нужно скопировать")
+print("Укажите расширение файлов, которые нужно скопировать\n(по умолчанию pdf)")
 file_extension = input()
 if not file_extension:
     file_extension = 'pdf'
 
-print("Укажите путь к папке, в которую выполнить копирование")
+print("Укажите путь к папке, в которую выполнить копирование\n(по умолчанию каталог _new, в директории скрипта")
 new_dir_path = input()
 if not new_dir_path:
     new_dir_path = os.path.join(host_dir_path, '_new')
@@ -26,7 +26,11 @@ if not os.path.exists(new_dir_path):
     os.mkdir(new_dir_path)
 
 #Копирование файлов pdf
+counter = 0
 for e in all_pdf_files:
     new_file_path = os.path.join(new_dir_path, e.split('\\')[-1])
     if not os.path.exists(new_file_path):
         shutil.copy(e, new_file_path)
+        counter +=1
+
+print(f"Скрипт закончил работу, скопировано {counter} файлов")
